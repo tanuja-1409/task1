5cae9fe97fa27be28c8a050cdbcb878e269dde0b
tanuja-1409 <tanujadhav1409@gmail.com>
2026-02-18 18:49:31 +0530

    Create CountVowels.java

---
+++ CountVowels.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CountVowels {
+    static int countVowels(String str) {
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = Character.toLowerCase(str.charAt(i));
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+                count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String text = sc.nextLine();
+
+        System.out.println("Number of vowels: " + countVowels(text));
+    }
+}
